e4ecf407a0172eaf3b63ed25b3c1fc5e155438c2
euclidesseg <euder0206@gmail.com>
2023-02-05 22:24:49 -0500

    se termina el post put y delete

--- WebApi/WebApi/Controllers/ProductoController.cs
+++ WebApi/WebApi/Controllers/ProductoController.cs
@@ -7,7 +7,7 @@ namespace TiendaApi.Controllers
     [ApiController]
     [Route("api/productos")]
     /* esto indicaria la ruta de nuestra api a la que va a apuntar*/
-    public class ProductoController
+    public class ProductoController : ControllerBase
     {
         /*indicando que voy a consumir la data y traere la informacion con httget*/
         [HttpGet]
@@ -26,11 +26,33 @@ namespace TiendaApi.Controllers
         /* el frombody indica que es el cuerpo del metodo es decir se enviara la informacion atravez de un cuerpo json que representa
          * al modelado de productos*/
         [HttpPost]
-        public async Task PostProducto([FromBody] Mproductos parametros)
+        public async Task Post([FromBody] Mproductos parametros)/*indicamos que enviaremos los datos atravez de fromBody*/
         {
             var funcion = new ProductsDTO();
             await funcion.AddProducts(parametros);
         }
 
+
+        //Actualizando un registro con HttPut
+        [HttpPut("{Id}")] // para poder actualizar un registro siempre necesitamos enviarle un parametro a travez de la url para poder editar el registro
+        public async Task<ActionResult> Put(int Id, [FromBody] Mproductos parameters) //indicamos que el id del parametro va a llgar atravez del fronBody
+            /* este metodo va a pedir un id de parametro que llega atravez de la url y va a pedir un objeto de tipo Mproductos*/
+        {
+            var funcion = new ProductsDTO();
+            parameters.id = Id;
+            await funcion.EditarProductos(parameters);
+            return NoContent(); //para poder usar este metodo debemos heredar de basecontrollers
+        }
+
+        [HttpDelete("{Id}")] /*Para hacer un delete tambien necesitamos pasar un parametro por la ruta*/
+        public async Task<ActionResult> Delete(int Id)
+        {
+            var funcion = new ProductsDTO();
+            var parameters = new Mproductos();
+            parameters.id = Id;
+            await funcion.EliminarProductos(parameters);
+            return NoContent();
+
+        }
     }
 }
--- WebApi/WebApi/DTOs/ProductsDTO.cs
+++ WebApi/WebApi/DTOs/ProductsDTO.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
 using System.Data.SqlClient;
 using TiendaApi.Conexion;
 using TiendaApi.Models;
@@ -11,6 +12,7 @@ namespace TiendaApi.DTOs
     {
         ConexionBD  cnx = new ConexionBD();
 
+        /*Mostrar los registros*/
         public async Task <List<Mproductos>> MostrarProductos()
             /*indicamos que retornaremos una lista de productos cada vez que se llame a la funcion*/
         {
@@ -43,22 +45,67 @@ namespace TiendaApi.DTOs
             return lista;
         }
 
-        public async Task AddProducts(Mproductos parametros)// cada que el usuario consuma esta funcion tendra que enviar un objeto de parametros
+      
+        /*agregar registros*/
+        public async Task AddProducts(Mproductos parametros)// cada que el usuario consuma esta funcion tendra que enviar un objeto de Mproductos
         {
             using (var sql = new SqlConnection(cnx.getConection()))
             {
-                /* a continuacion hacemos nuestro comando nuevamente*/
+                /* a continuacion hacemos nuestro comando para agregar productos*/
                 using(var cmd = new SqlCommand("[dbo].[InsertProduct]" ,sql))
                 {
                     cmd.CommandType = CommandType.StoredProcedure;
                     /* ahora indicamos que tambien tiene parametros*/
-                    cmd.Parameters.AddWithValue("descripcion", parametros.descripcion); //indicamos que este procedimiento tendra paramtros para agrear
-                    cmd.Parameters.AddWithValue("precio", parametros.precio);
+                    cmd.Parameters.AddWithValue("@descripcion", parametros.descripcion); //indicamos que este procedimiento tendra paramtros para agrear
+                    cmd.Parameters.AddWithValue("@precio", parametros.precio);
                     await sql.OpenAsync();
                     await cmd.ExecuteNonQueryAsync();
                 }
             }
 
         }
+
+
+        /*Actualizamos los datos  */
+        public async Task EditarProductos(Mproductos parametros)// cada que el usuario consuma esta funcion tendra que enviar un objeto de Mproductos
+        {
+            using (var sql = new SqlConnection(cnx.getConection()))
+            {
+                /* a continuacion hacemos nuestro comando para editar productos*/
+                using (var cmd = new SqlCommand("[dbo].[UpdateProduct]", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    /* ahora indicamos que tambien tiene parametros*/
+
+                    cmd.Parameters.AddWithValue("@id", parametros.id); //indicamos que este procedimiento tendra paramtros para agrear
+                    cmd.Parameters.AddWithValue("@precio", parametros.precio);// vamoa a actualizar solamanet el precio del producto seleccionado
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                } 
+            }
+
+        }
+
+
+
+        /*Eliminar un registro*/
+        public async Task EliminarProductos(Mproductos parameters)
+        /*indicamos que retornaremos una lista de productos cada vez que se llame a la funcion*/
+        {
+            using (var sql = new SqlConnection(cnx.getConection())) // la funcion using me cierra la conexion automaticamente
+            {
+
+                /*a continuacion consumiremos el procedimiento almacenado*/
+                using (var cmd = new SqlCommand("[dbo].[DeleteProduct]", sql))
+                {
+                    /*indicamos que se trata de un procedimiento almacenadao*/
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parameters.id);
+                    await sql.OpenAsync(); //para habrir la conexion
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
     }
 }
